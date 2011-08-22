4da12e27b11823898f3a5b4fde6a18385082eeba
Andrew Regan <aregan@gmail.com>
2011-08-22 23:40:15 +0100

    More thorough testing of consistency between Hib and ES
    Signed-off-by: Andrew Regan <aregan@gmail.com>

--- src/test/java/org/elasticsearch/osem/hibernate/BasicHibTest.java
+++ src/test/java/org/elasticsearch/osem/hibernate/BasicHibTest.java
@@ -4,6 +4,7 @@
  */
 package org.elasticsearch.osem.hibernate;
 
+import java.util.Collection;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.util.Locale;
@@ -46,6 +47,13 @@ public class BasicHibTest
 {
 	private static Client		s_Client;
 
+	private final static String	IDX_ACTOR	= "actor";
+	private final static String	IDX_ARTICLE	= "testarticle";
+	private final static String	IDX_BLOG	= "blog";
+	private final static String	IDX_FEED	= "feed";
+
+	private final static String	TEST_TYPE	= "xxx";
+
 	private static final ESLogger	m_Logger = Loggers.getLogger( BasicHibTest.class );
 
 	/****************************************************************************
@@ -57,7 +65,7 @@ public class BasicHibTest
 
 		// Clear down...
 
-		s_Client.prepareDeleteByQuery("actor", "blog", "feed", "testarticle").setQuery( matchAllQuery() ).execute().actionGet();
+		s_Client.prepareDeleteByQuery( IDX_ACTOR, IDX_ARTICLE, IDX_BLOG, IDX_FEED).setQuery( matchAllQuery() ).execute().actionGet();
 	}
     
 	/****************************************************************************
@@ -137,46 +145,13 @@ public class BasicHibTest
 		theEM.getTransaction().commit();
 		theEM.clear();
 
-		//////////////////////////////////////////////////////////////////////////////////////
-
-		final ActorIF		theLoadedActor = theEM.createQuery("FROM Actor WHERE userName='aregan'", Actor.class).getSingleResult();
-		final BlogIF		theLoadedBlog = theEM.createQuery("FROM Blog WHERE url='http://www.poblish.org/blog/'", Blog.class).getSingleResult();
-		final FeedIF		theLoadedFeed = theEM.createQuery("FROM Feed WHERE url='http://www.poblish.org/blog/'", Feed.class).getSingleResult();
-
-		Assert.assertNotNull( theLoadedActor.getId() );
-		Assert.assertNotNull( theLoadedBlog.getId() );
-		Assert.assertNotNull( theLoadedFeed.getId() );
+		testSynchronisation(theEM);
 
 		//////////////////////////////////////////////////////////////////////////////////////
 
-		m_Logger.debug( "*** Refreshing...");
-
-		s_Client.admin().indices().prepareRefresh("actor").execute().actionGet();
-		s_Client.admin().indices().prepareRefresh("blog").execute().actionGet();
-		s_Client.admin().indices().prepareRefresh("feed").execute().actionGet();
-
-		m_Logger.debug( "*** Searching...");
-
-		final SearchResponse	theActorResp = s_Client.prepareSearch("actor").setQuery( idsQuery("xxx").addIds( String.valueOf( theLoadedActor.getId() ) ) ).execute().actionGet();
-
-		Assert.assertEquals( theActorResp.getHits().totalHits(), 1, "Wrong number of Actors (#1) for Id #" + theLoadedActor.getId());
-
-		final SearchResponse	theBlogResp = s_Client.prepareSearch("blog").setQuery( idsQuery("xxx").addIds( String.valueOf( theLoadedBlog.getId() ) ) ).execute().actionGet();
-
-		Assert.assertEquals( theBlogResp.getHits().totalHits(), 1, "Wrong number of Blogs (#1) for Id #" + theLoadedBlog.getId());
-
-		final SearchResponse	theFeedResp = s_Client.prepareSearch("feed").setQuery( idsQuery("xxx").addIds( String.valueOf( theLoadedFeed.getId() ) ) ).execute().actionGet();
-
-		Assert.assertEquals( theFeedResp.getHits().totalHits(), 1, "Wrong number of Feeds (#1) for Id #" + theLoadedFeed.getId());
-
-		//////////////////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////////////////
+		final ActorIF	theLoadedActor = theEM.createQuery("FROM Actor WHERE userName='aregan'", Actor.class).getSingleResult();
 
 		theEM.getTransaction().begin();
-
-		m_Logger.debug( "*** Removing...");
-
 		theEM.remove(theLoadedActor);
 		theEM.flush();
 		theEM.getTransaction().commit();
@@ -189,8 +164,12 @@ public class BasicHibTest
 
 		Assert.assertEquals( theActorResp2.getHits().totalHits(), 0, "Wrong number of Actors #2");
 
+		testSynchronisation(theEM);
+
 		//////////////////////////////////////////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////////////
+
+		final FeedIF	theLoadedFeed = theEM.createQuery("FROM Feed WHERE url='http://www.poblish.org/blog/'", Feed.class).getSingleResult();
 
 		theEM.getTransaction().begin();
 		theEM.remove(theLoadedFeed);
@@ -204,6 +183,106 @@ public class BasicHibTest
 		final SearchResponse	theFeedResp2 = s_Client.prepareSearch("feed").setQuery( matchAllQuery() ).execute().actionGet();
 
 		Assert.assertEquals( theFeedResp2.getHits().totalHits(), 0, "Wrong number of Feeds #2");
+
+		testSynchronisation(theEM);
+	}
+
+	/****************************************************************************
+	****************************************************************************/
+	private void testSynchronisation( final EntityManager inEM)
+	{
+		final Collection<TestArticle>	theArticles = inEM.createQuery("FROM TestArticle WHERE title='Seumas Milne'", TestArticle.class).getResultList();
+		final Collection<Actor>		theActors = inEM.createQuery("FROM Actor WHERE userName='aregan'", Actor.class).getResultList();
+		final Collection<Blog>		theBlogs = inEM.createQuery("FROM Blog WHERE url='http://www.poblish.org/blog/'", Blog.class).getResultList();
+		final Collection<Feed>		theFeeds = inEM.createQuery("FROM Feed WHERE url='http://www.poblish.org/blog/'", Feed.class).getResultList();
+
+		System.out.println("> Articles = " + theArticles);
+		System.out.println("> Actors   = " + theActors);
+		System.out.println("> Blogs    = " + theBlogs);
+		System.out.println("> Feeds    = " + theFeeds);
+
+		Assert.assertTrue( theArticles.size() <= 1 );
+		Assert.assertTrue( theActors.size() <= 1 );
+		Assert.assertTrue( theBlogs.size() <= 1 );
+		Assert.assertTrue( theFeeds.size() <= 1 );
+
+		s_Client.admin().indices().prepareRefresh( IDX_ACTOR, IDX_ARTICLE, IDX_BLOG, IDX_FEED).execute().actionGet();
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+		if (theArticles.isEmpty())
+		{
+			final SearchResponse	theZeroArticlesResp = s_Client.prepareSearch(IDX_ARTICLE).setQuery( matchAllQuery() ).execute().actionGet();
+
+			Assert.assertEquals( theZeroArticlesResp.getHits().totalHits(), 0, "Wrong number of Articles #2");
+		}
+		else
+		{
+			final ArticleIF		the1Article = theArticles.iterator().next();
+
+			Assert.assertNotNull( the1Article.getId() );
+
+			final SearchResponse	theActorResp = s_Client.prepareSearch(IDX_ARTICLE).setQuery( idsQuery(TEST_TYPE).addIds( String.valueOf( the1Article.getId() ) ) ).execute().actionGet();
+
+			Assert.assertEquals( theActorResp.getHits().totalHits(), 1, "Wrong number of Articles (#1) for Id #" + the1Article.getId());
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+		if (theActors.isEmpty())
+		{
+			final SearchResponse	theZeroActorsResp = s_Client.prepareSearch(IDX_ACTOR).setQuery( matchAllQuery() ).execute().actionGet();
+
+			Assert.assertEquals( theZeroActorsResp.getHits().totalHits(), 0, "Wrong number of Actors #2");
+		}
+		else
+		{
+			final ActorIF		the1Actor = theActors.iterator().next();
+
+			Assert.assertNotNull( the1Actor.getId() );
+
+			final SearchResponse	theActorResp = s_Client.prepareSearch(IDX_ACTOR).setQuery( idsQuery(TEST_TYPE).addIds( String.valueOf( the1Actor.getId() ) ) ).execute().actionGet();
+
+			Assert.assertEquals( theActorResp.getHits().totalHits(), 1, "Wrong number of Actors (#1) for Id #" + the1Actor.getId());
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+		if (theBlogs.isEmpty())
+		{
+			final SearchResponse	theZeroBlogsResp = s_Client.prepareSearch(IDX_BLOG).setQuery( matchAllQuery() ).execute().actionGet();
+
+			Assert.assertEquals( theZeroBlogsResp.getHits().totalHits(), 0, "Wrong number of Blogs #2");
+		}
+		else
+		{
+			final BlogIF		the1Blog = theBlogs.iterator().next();
+
+			Assert.assertNotNull( the1Blog.getId() );
+
+			final SearchResponse	theBlogResp = s_Client.prepareSearch(IDX_BLOG).setQuery( idsQuery(TEST_TYPE).addIds( String.valueOf( the1Blog.getId() ) ) ).execute().actionGet();
+
+			Assert.assertEquals( theBlogResp.getHits().totalHits(), 1, "Wrong number of Blogs (#1) for Id #" + the1Blog.getId());
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+
+		if (theFeeds.isEmpty())
+		{
+			final SearchResponse	theZeroFeedsResp = s_Client.prepareSearch(IDX_FEED).setQuery( matchAllQuery() ).execute().actionGet();
+
+			Assert.assertEquals( theZeroFeedsResp.getHits().totalHits(), 0, "Wrong number of Feeds #2");
+		}
+		else
+		{
+			final FeedIF		the1Feed = theFeeds.iterator().next();
+
+			Assert.assertNotNull( the1Feed.getId() );
+
+			final SearchResponse	theFeedResp = s_Client.prepareSearch(IDX_FEED).setQuery( idsQuery(TEST_TYPE).addIds( String.valueOf( the1Feed.getId() ) ) ).execute().actionGet();
+
+			Assert.assertEquals( theFeedResp.getHits().totalHits(), 1, "Wrong number of Feeds (#1) for Id #" + the1Feed.getId());
+		}
 	}
 
 	/****************************************************************************
